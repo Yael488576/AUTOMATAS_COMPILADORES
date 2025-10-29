f4ee3683de14d3f70247faac0ebebdb41556a30f
Sergio Yael <pa488576@uaeh.edu.mx>
2025-10-28 21:17:16 -0600

    Add files via upload

---
+++ TERCER PARCIAL/brutomate.h
@@ -0,0 +1,8 @@
+#define TOTAL_LINEAS 1
+	#define TOTAL_PALABRAS 2  
+	#define TOTAL_CARACTERES 3
+	#define PALABRAS_NI 4
+	#define PALABRAS_NUM 5
+	#define PALABRAS_B_TO 6
+	#define PALABRAS_JO 7
+	#define PALABRAS_DIGITOS 8
